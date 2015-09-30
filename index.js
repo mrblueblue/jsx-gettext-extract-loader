a81cfbd02004e0025101e8d961081a139bcc8650
Jonathan Huang <grokut@gmail.com>
2015-09-30 15:07:33 -0700

    feat/ loader with output query

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
+var path = require('path');
+var fs = require('fs');
+var loaderUtils = require('loader-utils');
+
 module.exports = function(source) {
-  var re = /__\(\'(.*)\'\)/g
-  var found = source.match(re);
-  if (found) { console.log(found) }
+
+  this.cacheable();
+
+  var pattern = /__\(\'(.*)\'\)/g
+  var translations = source.match(pattern);
+
+  var loaderOptions = loaderUtils.parseQuery(this.query);
+  var output = loaderOptions.hasOwnProperty('output') ? loaderOptions.output : __dirname
+
+  var translationsPath = path.resolve(output + '/jsx-translations.js');
+  this.addDependency(translationsPath);
+
+  if (translations) {
+
+    fs.readFile(translationsPath, 'utf8', function(err, data) {
+      if (err) {
+        translations = translations.join('\n');
+      } else {
+        translations = translations.concat(data.split('\n')).join('\n');
+      }
+
+      fs.writeFileSync(translationsPath, translations);
+    });
+  }
+
   return source;
 }
